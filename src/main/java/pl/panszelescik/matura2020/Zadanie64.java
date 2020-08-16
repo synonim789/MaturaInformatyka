2796173a8cafb5e92f4656210cc8a229e15fbb1a
PanSzelescik <szymonszelest@gmail.com>
2020-08-16 22:00:35 +0200

    Cleanup

--- src/main/java/pl/panszelescik/matura2020/Zadanie64.java
+++ src/main/java/pl/panszelescik/matura2020/Zadanie64.java
@@ -7,7 +7,6 @@ import pl.panszelescik.matura2020.base.Type;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Zadanie64 {
 
