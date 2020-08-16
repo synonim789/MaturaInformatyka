7729d2a5863308d9b0e9eb55f95ecbf89443a5ad
PanSzelescik <szymonszelest@gmail.com>
2020-08-16 17:58:39 +0200

    Zadanie 6.2

--- src/main/java/pl/panszelescik/api/DateUtils.java
+++ src/main/java/pl/panszelescik/api/DateUtils.java
@@ -2,10 +2,15 @@ package pl.panszelescik.api;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class DateUtils {
 
     public static LocalDate parse(String string, String pattern) {
         return LocalDate.parse(string, DateTimeFormatter.ofPattern(pattern));
+    }
+
+    public static long getFullDays(LocalDate before, LocalDate after) {
+        return ChronoUnit.DAYS.between(before, after) - 1;
     }
 }
