0e2687a33687667878f3f9274efb43fdd4d01905
PanSzelescik <szymonszelest@gmail.com>
2020-08-16 22:00:22 +0200

    Zaladunek i wyladunek

--- src/main/java/pl/panszelescik/matura2020/Zadanie64.java
+++ src/main/java/pl/panszelescik/matura2020/Zadanie64.java
@@ -2,17 +2,29 @@ package pl.panszelescik.matura2020;
 
 import pl.panszelescik.api.*;
 import pl.panszelescik.matura2020.base.Statek;
+import pl.panszelescik.matura2020.base.Type;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Zadanie64 {
 
     public static void main(String[] args) {
         List<Statek> statki = FileUtils.mapFile("2020/Dane_PR2/statek.txt", Statek::new, 1);
+        FileUtils.write("2020_zadanie64.txt", writer -> {
+            zaladunek(writer, statki);
+            wyladunek(writer, statki);
+        });
+    }
+
+    public static void zapisz(FileUtils.MyWriter writer, List<Statek> statki, Type type) {
+        writer.writeLine(type);
+        System.out.println(type);
         Map<String, Integer> map = statki.stream()
                 .filter(statek -> statek.towar.equals("T5"))
+                .filter(statek -> statek.type.equals(type))
                 .filter(statek -> statek.date.isAfter(DateUtils.parse("2016-01-01", "yyyy-MM-dd")) && statek.date.isBefore(DateUtils.parse("2018-12-18", "yyyy-MM-dd")))
                 .sorted(Comparator.comparing(statek -> statek.date))
                 .collect(CollectionUtils.toSumMap(statek -> DateUtils.format(statek.date, "yyyy-MM"), statek -> statek.waga));
@@ -21,8 +33,23 @@ public class Zadanie64 {
                 map.putIfAbsent(i + "-" + NumberUtils.prettyNumber(j), 0);
             }
         }
-        FileUtils.writeStream("2020_zadanie64.txt", map.entrySet()
+        map.entrySet()
                 .stream()
-                .sorted(Map.Entry.comparingByKey()));
+                .sorted(Map.Entry.comparingByKey())
+                .map(entry -> entry.getKey() + "," + entry.getValue())
+                .forEach(string -> {
+                    writer.writeLine(string);
+                    System.out.println(string);
+                });
+        writer.write("\n");
+        System.out.println(" ");
+    }
+
+    public static void zaladunek(FileUtils.MyWriter writer, List<Statek> statki) {
+        zapisz(writer, statki, Type.ZALADUNEK);
+    }
+
+    public static void wyladunek(FileUtils.MyWriter writer, List<Statek> statki) {
+        zapisz(writer, statki, Type.WYLADUNEK);
     }
 }
