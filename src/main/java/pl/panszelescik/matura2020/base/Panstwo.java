506421985662d53be83a7e014b7716841e091881
PanSzelescik <szymonszelest@gmail.com>
2020-08-16 15:15:07 +0200

    Cleanup

--- src/main/java/pl/panszelescik/matura2020/base/Panstwo.java
+++ src/main/java/pl/panszelescik/matura2020/base/Panstwo.java
@@ -8,18 +8,12 @@ public class Panstwo {
     public final String nazwa;
     public final String kontynent;
     public final double populacja;
-    public List<Jezyk> jezyki = new ArrayList<>();
 
     public Panstwo(String line) {
         String[] array = line.split("\t");
         this.nazwa = array[0];
         this.kontynent = array[1];
         this.populacja = Double.parseDouble(array[2].replace(",", "."));
-    }
-
-    public Panstwo addJezyk(Jezyk jezyk) {
-        this.jezyki.add(jezyk);
-        return this;
     }
 
     @Override
