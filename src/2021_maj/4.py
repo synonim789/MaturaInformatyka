c3cc7d054d7029c187ded191b66bb782c47faa12
PanSzelescik <panszelescik@gmail.com>
2021-10-03 15:28:03 +0200

    Unneeded files in python

--- src/2021_maj/4.py
+++ src/2021_maj/4.py
@@ -8,7 +8,7 @@ def nextLetter(char):  # Funkcja obliczająca następną literę, jeśli zostani
 
 # Odczyt pliku
 file = open('../../resources/2021_maj/DANE_2105/instrukcje.txt', 'r', encoding='utf8')  # Wczytanie pliku do odczytu w utf8
-#file = open('../../resources/2021_maj/DANE_2105/przyklad.txt', 'r', encoding='utf8')  # Wczytanie pliku przykłądowego do odczytu w utf8
+#file = open('../../resources/2021_maj/DANE_2105/przyklad.txt', 'r', encoding='utf8')  # Wczytanie pliku przykładowego do odczytu w utf8
 array = list(map(lambda x: x.strip().split(' '), file))  # Dzielenie pliku na tablicę tablic, strip usuwa zbędne \n oraz spacje, split dzieli linię
 file.close()  # Zamknięcie pliku
 
@@ -47,13 +47,13 @@ for instrukcja, litera in array:  # Taki for pozwala od razu pobierać dany elem
         last_count = 1
     elif last_name == instrukcja:  # Jeśli instrukcja się powtarza dodaj 1
         last_count = last_count + 1
-    else:  # Jeśli instrukcja nie powtarza się, dodaj do tablicy i ustaw 1
+    else:  # Jeśli instrukcja nie powtarza się, dodaj do tablicy i ustaw nową instrukcję z 1 powtórzeniem
         powtorzenia.append([last_name, last_count])
         last_name = instrukcja
         last_count = 1
 powtorzenia.append([last_name, last_count])  # Koniec instrukcji, dodaj do tablicy ostatnie powtórzenie
 
-sorted_powtorzenia = sorted(powtorzenia, key=lambda x: x[1], reverse=True)  # Funkcja sortująca, biorąca 2 element tablicy w tablicy czyli liczbę
+sorted_powtorzenia = sorted(powtorzenia, key=lambda x: x[1], reverse=True)  # Funkcja sortująca, biorąca pod uwagę 2 element tablicy w tablicy czyli liczbę
 print(f'rodzaj instrukcji - {sorted_powtorzenia[0][0]}, długość ciągu - {sorted_powtorzenia[0][1]}')  # Print, rozwiązanie 4.2
 
 
@@ -67,7 +67,7 @@ for instrukcja, litera in array:  # Taki for pozwala od razu pobierać dany elem
         else:  # Jeśli litery nie ma w słowniku, dodaj ją i ustaw 1
             dopisywania[litera] = 1
 
-sorted_dopisywania = sorted(dopisywania.items(), key=lambda x: x[1], reverse=True)  # Funkcja sortująca, biorąca liczbę dopisań
+sorted_dopisywania = sorted(dopisywania.items(), key=lambda x: x[1], reverse=True)  # Funkcja sortująca, biorąca pod uwagę liczbę dopisań
 print(f'litera {sorted_dopisywania[0][0]}, dopisywana {sorted_dopisywania[0][1]} razy')  # Print, rozwiązanie 4.3
 
 
